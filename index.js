577a45a71f697fc6dd5ec74d0d8fa8784fe5d814
Serhii Tomashkevich <19tomat@gmail.com>
2021-05-08 00:05:54 +0300

    +add:imports from tasks

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-import './task1.js';
-import './task2.js';
-import './task3.js';
+import a from './tasks/task1.js';
+import b from './tasks/task2.js';
+import c from './tasks/task3.js';
+
+document.querySelectorAll('button').forEach((btn, inx) =>
+  btn.addEventListener('click', () => {
+    console.clear();
+    switch (inx) {
+      case 0:
+        a();
+        break;
+      case 1:
+        b();
+        break;
+      case 2:
+        c();
+        break;
+      default:
+        break;
+    }
+  }),
+);
